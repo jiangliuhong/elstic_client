b04aee656214ff5faae310851e493c4c71d76616
jiangliuhong <ja_rome@163.com>
2025-08-11 23:18:14 +0800

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:elasitc_client/main1.dart';
+import 'package:elasitc_client/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
